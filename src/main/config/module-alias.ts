678d91557342353ccf5283e85e1a89460eae665b
Francisco Barros <franciscobarros.dev@gmail.com>
2021-10-04 15:15:15 -0300

    add eslint

--- src/main/config/module-alias.ts
+++ src/main/config/module-alias.ts
@@ -1,3 +1,3 @@
-import { addAlias } from "module-alias";
-import { resolve } from "path";
-addAlias("@", resolve("dist"));
+import { addAlias } from 'module-alias'
+import { resolve } from 'path'
+addAlias('@', resolve('dist'))
