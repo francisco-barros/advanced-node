d776997ca228e0fa424a474428c16afcb8110ec3
Francisco Barros <franciscobarros.dev@gmail.com>
2021-10-05 11:35:50 -0300

    add facebook authentication feature

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,23 @@
 module.exports = {
 
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
-  coverageProvider: "babel",
+  coverageProvider: 'babel',
 
   moduleNameMapper: {
-    "@/tests/(.+)": "<rootDir>/tests/$1",
-    "@/(.+)": "<rootDir>/src/$1"
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
 
   },
 
   roots: [
-    "<rootDir>/src",
-    "<rootDir>/tests"
+    '<rootDir>/src',
+    '<rootDir>/tests'
   ],
 
   transform: {
-    "\\.ts$": "ts-jest"
-  },
-};
+    '\\.ts$': 'ts-jest'
+  }
+}
